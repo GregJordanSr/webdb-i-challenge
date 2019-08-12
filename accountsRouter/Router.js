b56938852e32314709bd2c33f29ab861864eda84
Greg Jordan <gregljordan@gmail.com>
2019-08-12 19:03:58 -0400

    Completed the queries

---
+++ accountsRouter/Router.js
@@ -0,0 +1,31 @@
+const express = require('express');
+
+
+const db = require('../data/dbConfig.js');
+
+const router = express.Router();
+
+
+
+router.get('/', (req, res) => {
+    
+});
+
+router.get('/:id', (req, res) => {
+    
+});
+
+router.post('/', (req, res) => {
+    
+});
+
+router.put('/', (req, res) => {
+    
+});
+
+router.delete('/', (req, res) => {
+    
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
-
+const router = require('./accountsRouter/Router.js')
 const server = express();
 
 server.use(express.json());
+
+server.use('/api/accounts', router);
+
+// server.get('/', (req, res) => {
+    
+// });
+
+// server.get('/:id', (req, res) => {
+    
+// });
+
+// server.post('/', (req, res) => {
+    
+// });
+
+// server.put('/', (req, res) => {
+    
+// });
+
+// server.delete('/', (req, res) => {
+    
+// });
 
 module.exports = server;
