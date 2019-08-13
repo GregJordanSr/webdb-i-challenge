c8959c629bff7b896aeed81a4fb9c93c6cd27269
Greg Jordan <gregljordan@gmail.com>
2019-08-12 20:01:07 -0400

    Completed both GET / request

--- accountsRouter/Router.js
+++ accountsRouter/Router.js
@@ -17,10 +17,22 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-    
+    db("accounts")
+    .where({ id: req.params.id })
+        .then((account) => {
+            if (account) {
+                res.status(200).json(account);
+            } else {
+                res.status(404).json({ message: "Could not retrieve specified account"});
+            }
+        })
+        .catch((err) => {
+            res.status(500).json({  message: "There was an error with the server" });
+        });
 });
 
 router.post('/', (req, res) => {
+    db("accounts").insert(id, account)
     
 });
 
