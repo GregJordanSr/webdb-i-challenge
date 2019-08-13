d253b81366a7857ec980983c66b2c168becfa9eb
Greg Jordan <gregljordan@gmail.com>
2019-08-12 21:58:26 -0400

    Completed Post and Put methods

--- accountsRouter/Router.js
+++ accountsRouter/Router.js
@@ -19,6 +19,7 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     db("accounts")
     .where({ id: req.params.id })
+    .first()
         .then((account) => {
             if (account) {
                 res.status(200).json(account);
@@ -32,12 +33,35 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    db("accounts").insert(id, account)
-    
-});
+    const {name, budget} = req.body;
+    accountBody = req.body;
+   
+        db('accounts').insert(accountBody)
+           
+        .then((account) => {
+                res.status(201).json(account)
+          }).catch(err => {
+                res.status(500).json({message: "There was an error with server"  });
+    })
+})
 
-router.put('/', (req, res) => {
-    
+
+router.put('/:id', (req, res) => {
+    const change = req.body;
+
+    db('accounts')
+        .where({ id: req.params.id })
+        .update(change)
+            .then((record) => {
+                if (!record > 0) {
+                   res.status(404).json({ message: " This account was not updated. "})
+                } else {
+                    res.status(200).json(change);
+                }
+                
+            }).catch((err) => {
+                res.status(500).json({ message: "There was an error on the server"});
+            });
 });
 
 router.delete('/', (req, res) => {
